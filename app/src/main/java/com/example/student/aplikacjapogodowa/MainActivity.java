d867e96284c97572ffa1249bbe1e093335631b03
unknown <student@IBLAB732P-19.ib.polsl.pl>
2017-12-13 15:24:52 +0100

    dodane findViewById

--- app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.student.aplikacjapogodowa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        EditText wpisz = findViewById(R.id.podaj);
+        Button zmien = findViewById(R.id.zmien_miejsce);
+        TextView miasto = findViewById(R.id.NazwaMiasta);
+
+        Button lista = findViewById(R.id.lista_miejsc);
+    }
 }
