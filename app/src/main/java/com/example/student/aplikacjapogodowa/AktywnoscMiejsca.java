069409b3a2feeb370fe6c2412e77cddc8f2ba283
MartaDziemba <34282722+MartaDziemba@users.noreply.github.com>
2017-12-19 22:06:26 +0100

    dodano AktywnoscMiejsca, setOnClickListener, getText, intent i budle

---
+++ app/src/main/java/com/example/student/aplikacjapogodowa/AktywnoscMiejsca.java
@@ -0,0 +1,13 @@
+package com.example.student.aplikacjapogodowa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AktywnoscMiejsca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktywnosc_miejsca);
+    }
+}
--- app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.aplikacjapogodowa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,10 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText wpisz = findViewById(R.id.podaj);
+        final EditText wpisz = findViewById(R.id.podaj);
         Button zmien = findViewById(R.id.zmien_miejsce);
-        TextView miasto = findViewById(R.id.NazwaMiasta);
+        final TextView miasto = findViewById(R.id.NazwaMiasta);
 
         Button lista = findViewById(R.id.lista_miejsc);
+
+        zmien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inne_miejsce = wpisz.getText().toString();
+                miasto.setText(inne_miejsce);
+            }
+        });
+
+        lista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AktywnoscMiejsca.class);
+                startActivity(intent);
+                Bundle bundle = new Bundle();bundle.putString("wpisz","miasto");
+                intent.putExtras(bundle);
+            }
+        });
+
     }
 }
