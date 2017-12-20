ce93413edcc542903305f4356a3648368d8761f4
unknown <student@IBLAB732P-19.ib.polsl.pl>
2017-12-20 14:51:50 +0100

    @BindView ustawione

--- app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
@@ -3,21 +3,38 @@ package com.example.student.aplikacjapogodowa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.podaj)
+    EditText wpisz;
+
+    @BindView(R.id.zmien_miejsce)
+    Button zmien;
+
+    @BindView(R.id.NazwaMiasta)
+    TextView miasto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        final EditText wpisz = findViewById(R.id.podaj);
-        Button zmien = findViewById(R.id.zmien_miejsce);
-        final TextView miasto = findViewById(R.id.NazwaMiasta);
+        //final EditText wpisz = findViewById(R.id.podaj);
+        //Button zmien = findViewById(R.id.zmien_miejsce);
+        //final TextView miasto = findViewById(R.id.NazwaMiasta);
+
 
         Button lista = findViewById(R.id.lista_miejsc);
 
